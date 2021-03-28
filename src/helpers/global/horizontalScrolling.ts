dde2d2de2bf3534cd0b2af69f54d5343c0b654c0
Alessandro Ramos <70819804+alessandrohenriqueramos@users.noreply.github.com>
2021-03-28 09:13:34 -0300

    create horizontalScrolling

---
+++ src/helpers/global/horizontalScrolling.ts
@@ -0,0 +1,23 @@
+export const horizontalScrolling = (element: HTMLElement) => {
+	const scrollHorizontally = (e: WheelEvent) => {
+		e.preventDefault();
+
+		const delta = Math.max(-1, Math.min(1, e.deltaY || -e.detail));
+
+		element.scrollLeft -= delta * 40;
+	};
+
+	// IE9, Chrome, Safari, Opera
+	element.addEventListener(
+		"mousewheel",
+		scrollHorizontally as EventListenerOrEventListenerObject,
+		false,
+	);
+
+	// Firefox
+	element.addEventListener(
+		"DOMMouseScroll",
+		scrollHorizontally as EventListenerOrEventListenerObject,
+		false,
+	);
+};
