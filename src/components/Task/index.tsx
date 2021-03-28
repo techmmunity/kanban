993c66702ab6a8b7df76d58352952d0ea94ccc6d
Alessandro Ramos <70819804+alessandrohenriqueramos@users.noreply.github.com>
2021-03-28 14:05:36 -0300

    update task

--- src/components/Task/index.tsx
+++ src/components/Task/index.tsx
@@ -1,7 +1,3 @@
-import { useEffect, useRef } from "react";
-
-import { stopPropagationHorizontalScrolling } from "helpers/global/stopPropagationHorizontalScrolling";
-
 import { Container } from "./styles";
 
 interface Props {
@@ -9,18 +5,8 @@ interface Props {
 	color?: string;
 }
 
-export const Task: React.FC<Props> = ({ title, color }) => {
-	const containerRef = useRef<HTMLLIElement>(null);
-
-	useEffect(() => {
-		if (containerRef.current) {
-			stopPropagationHorizontalScrolling(containerRef.current);
-		}
-	}, []);
-
-	return (
-		<Container color={color} ref={containerRef}>
-			<p>{title}</p>
-		</Container>
-	);
-};
+export const Task: React.FC<Props> = ({ title, color }) => (
+	<Container color={color}>
+		<p>{title}</p>
+	</Container>
+);
--- src/components/Task/styles.ts
+++ src/components/Task/styles.ts
@@ -6,7 +6,11 @@ import { isDarkColor } from "helpers/assets/isDarkColor";
 export const Container = styled.li<{ color?: string }>`
 	background: ${({ color }) => color || Colors.primary};
 	color: ${({ color }) =>
-		color && isDarkColor(color) ? Colors.primary : Colors.secondary};
+		color
+			? isDarkColor(color)
+				? Colors.primary
+				: Colors.secondary
+			: Colors.secondary};
 	border-radius: 3px;
 	padding: 0.8rem 0.5rem;
 	margin: 0.8rem 0;
