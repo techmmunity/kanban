9a4c14053315453f0f2be7b310cdb76592f7dfcd
Alessandro Ramos <70819804+alessandrohenriqueramos@users.noreply.github.com>
2021-03-28 09:41:32 -0300

    add Head

--- src/components/Panel/index.tsx
+++ src/components/Panel/index.tsx
@@ -1,14 +1,21 @@
+import Head from "next/head";
+
 import { Board } from "components/Board";
 
 import { Container, Header } from "./styles";
 
 export const Panel = () => (
-	<Container>
-		<Header>
-			<div>
-				<h1>Panel 1</h1>
-			</div>
-		</Header>
-		<Board />
-	</Container>
+	<>
+		<Head>
+			<title>Painel 1 · Kanban</title>
+		</Head>
+		<Container>
+			<Header>
+				<div>
+					<h1>Panel 1</h1>
+				</div>
+			</Header>
+			<Board />
+		</Container>
+	</>
 );
