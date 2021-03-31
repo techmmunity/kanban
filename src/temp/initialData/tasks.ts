9e728b0aa0a1209affc84808ea5ce48f75bc261a
Alessandro Ramos <70819804+alessandrohenriqueramos@users.noreply.github.com>
2021-03-30 22:31:11 -0300

    create tasks

---
+++ src/temp/initialData/tasks.ts
@@ -0,0 +1,65 @@
+import { ITask } from "types/interfaces/Task";
+
+export const tasks: Array<ITask> = [
+	{
+		id: "task1",
+		title: "Task 1",
+	},
+];
+
+export const task1: Array<ITask> = [
+	{
+		id: "task1",
+		title:
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo sequi ullam necessitatibus iste, eveniet assumenda repellat ad commodi rem nulla esse molestias dolor ducimus consequatur distinctio voluptatibus tempore optio consectetur.",
+	},
+	{
+		id: "task2",
+		title: "Task 2",
+	},
+	{
+		id: "task3",
+		title:
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo sequi ullam necessitatibus iste, eveniet assumenda repellat ad commodi rem nulla esse molestias dolor ducimus consequatur distinctio voluptatibus tempore optio consectetur.",
+	},
+	{
+		id: "task4",
+		title: "Task 4",
+	},
+	{
+		id: "task5",
+		title: "Task 5",
+	},
+	{
+		id: "task6",
+		title:
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo sequi ullam necessitatibus iste, eveniet assumenda repellat ad commodi rem nulla esse molestias dolor ducimus consequatur distinctio voluptatibus tempore optio consectetur.",
+	},
+];
+
+export const task2: Array<ITask> = [
+	{
+		id: "task1",
+		title:
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo sequi ullam necessitatibus iste, eveniet assumenda repellat ad commodi rem nulla esse molestias dolor ducimus consequatur distinctio voluptatibus tempore optio consectetur.",
+	},
+	{
+		id: "task2",
+		title: "Task 2",
+	},
+	{
+		id: "task3",
+		title: "Task 3",
+	},
+];
+
+export const task3: Array<ITask> = [
+	{
+		id: "task1",
+		title: "Task 1",
+	},
+	{
+		id: "task2",
+		title: "Task 2",
+	},
+];
