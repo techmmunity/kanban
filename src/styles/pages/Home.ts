581a12b2a28c7744e62b0709cc919bcfdf73e7a9
Alessandro Ramos <70819804+alessandrohenriqueramos@users.noreply.github.com>
2021-03-30 22:23:30 -0300

    update style

--- src/styles/pages/Home.ts
+++ src/styles/pages/Home.ts
@@ -1,3 +1,22 @@
 import styled from "styled-components";
 
-export const Container = styled.main``;
+export const Container = styled.main`
+	> section {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		min-height: 100vh;
+
+		> div {
+			display: flex;
+			flex-direction: column;
+			justify-content: center;
+		}
+	}
+`;
+
+export const Title = styled.h1`
+	font-size: 4.5rem;
+	padding: 2rem 1.25rem;
+`;
