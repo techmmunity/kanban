82b8e9b5b3a3f37afc9d188f964152ae7ba6b78c
Alessandro Ramos <70819804+alessandrohenriqueramos@users.noreply.github.com>
2021-04-02 10:51:59 -0300

    add watermark

--- src/styles/pages/Board.ts
+++ src/styles/pages/Board.ts
@@ -12,12 +12,30 @@ const iconCSS = css`
 
 export const Container = styled.main<{ background?: string }>`
 	> section {
+		position: relative;
 		min-height: 100vh;
 		height: 100vh;
 		max-height: 100vh;
 		background: ${({ background }) => background && `url(${background})`};
 		background-position: center;
 		background-size: cover;
+		z-index: 1;
+
+		&::before {
+			position: absolute;
+			top: 0;
+			left: 0;
+			bottom: 0;
+			right: 0;
+			content: "Kanban";
+			display: flex;
+			align-items: center;
+			justify-content: center;
+			color: ${`${Colors.white}90`};
+			font-size: 15.625em;
+			font-weight: bold;
+			z-index: -1;
+		}
 
 		> header {
 			display: flex;
@@ -25,6 +43,7 @@ export const Container = styled.main<{ background?: string }>`
 			justify-content: space-between;
 			height: 5.2rem;
 			padding: 1rem 0.5rem;
+			z-index: 1;
 
 			> div {
 				display: flex;
