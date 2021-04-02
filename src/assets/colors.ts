3f73185ec5d3afc4501b2a4d49646fc6ae0d8103
Alessandro Ramos <70819804+alessandrohenriqueramos@users.noreply.github.com>
2021-04-02 18:31:36 -0300

    update colors

--- src/assets/colors.ts
+++ src/assets/colors.ts
@@ -11,6 +11,7 @@ export const Colors = {
 	"background-create-task-hover": "#5aac44",
 	"background-icon-container": "#16161890",
 	"background-icon-container-hover": "#0079bf",
+	"background-title": "#16161890",
 	scroll: "#f0f0f150",
 	"scroll-scrollbar": "#00000080",
 	white: "#ffffff",
