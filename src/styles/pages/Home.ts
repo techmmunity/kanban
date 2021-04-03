bb9076aa1ec239f6e942917875ff9fcc53c7dc12
Alessandro Ramos <70819804+alessandrohenriqueramos@users.noreply.github.com>
2021-04-02 22:00:26 -0300

    update home

--- src/styles/pages/Home.ts
+++ src/styles/pages/Home.ts
@@ -6,15 +6,9 @@ export const Container = styled.main`
 	align-items: center;
 	justify-content: center;
 	min-height: 100vh;
-
-	> section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-	}
 `;
 
 export const Title = styled.h1`
 	font-size: 4.5rem;
-	padding: 1.5rem 1.2rem;
+	margin: 1.2rem;
 `;
